94f4d2d038203974795113c6a3c4ad541e7518f4
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-09-16 15:55:30 +0200

    Update RabbitMQ.c

--- RabbitMQ.c
+++ RabbitMQ.c
@@ -3,12 +3,21 @@
 WSADATA data;
 SOCKET sock;
 SOCKADDR_IN adres;
-char x[50240];
-char serverDat[20240];
-char overview[] = "GET /api/consumers HTTP/1.1\r\n"
+int itt;
+char line;
+char x[100240];
+char serverDat[100240];
+char overview[] = "GET /api/overview HTTP/1.1\r\n"
 "authorization: Basic ZXVyb3RlYzpldXJvdGVj\r\n"
 "Host: 172.16.160.218:15672\r\n\r\n"
 ;
+void addSpace(){
+	return;
+	for(int i = 0;i < line;i++){
+		serverDat[itt] = 'x';
+		itt++;
+	}
+}
 int main(){
 	WORD ver = MAKEWORD(2, 2);
 	WSAStartup(ver, &data);
@@ -18,20 +27,32 @@ int main(){
 	adres.sin_addr.S_un.S_addr = htonl(2886770906);
 	connect(sock,(SOCKADDR*)&adres,sizeof(adres));
 	send(sock,overview,sizeof(overview) - 1	,0);
-	recv(sock,x,20240,0);
-	int itt = 0;
-	printf("%s",x);
+	recv(sock,x,100240,0);
+
 	for(int i = 0;i < sizeof(x);i++){
-		if(x[i] == '"'){
-			i++;
-			while(x[i] != '"'){
-				serverDat[itt] = x[i];
-				i++;
-				itt++;
-			}
-			serverDat[itt] = '\n';
+		serverDat[itt] = x[i];
+		switch(x[i]){
+		case ',':
 			itt++;
+			serverDat[itt] = '\n';
+			addSpace();
+			break;
+		case ']':
+		case '}':
+			itt++;
+			serverDat[itt] = '\n';
+			line-=4;
+			addSpace();
+			break;
+		case '[':
+		case '{':
+			itt++;
+			serverDat[itt] = '\n';
+			line+=4;
+			addSpace();
+			break;
 		}
+		itt++;
 	}	
 	printf("%s",serverDat);
 }
